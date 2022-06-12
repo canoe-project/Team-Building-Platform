b4b6042ec0d5093751bb25b3b211149c212533b1
canoe <ehgus430880@gmail.com>
2022-06-12 12:52:43 +0900

    add search link

---
+++ pages/contest/passToArticle.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const reqArticlePath = async (contestID) => {
+  const article = await fetch(
+    `${process.env.HOSTNAME}/api/article/Contest/Get/${contestID}/articleTo`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  ).then((response) => {
+    return response.json();
+  });
+
+  return article;
+};
+
+const ContestIndex = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { id } = router.query;
+    reqArticlePath(id).then((id) => {
+      router.push(`/contest/Read/${id.article_id}`, undefined, {
+        shallow: true,
+      });
+    });
+    // Always do navigations after the first render
+  }, [router]);
+
+  useEffect(() => {
+    // The counter changed!
+  }, [router.query.counter]);
+  return <></>;
+};
+
+export default ContestIndex;
